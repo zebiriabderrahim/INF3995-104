8e5c3563397f584a6112ad9bddccd158944ad294
abderrahim zebiri <abderrahim.zebiri@polymtl.ca>
2023-09-28 14:29:11 +0000

    feat:add limo_description package

--- embedded/gz_sim_ws/build/catkin_generated/setup_cached.sh
+++ embedded/gz_sim_ws/build/catkin_generated/setup_cached.sh
@@ -8,6 +8,7 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/abderrahim/INF3995-104/embedded/gz_sim_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/abderrahim/INF3995-104/embedded/gz_sim_ws/devel/lib:$LD_LIBRARY_PATH"
 export PWD='/home/abderrahim/INF3995-104/embedded/gz_sim_ws/build'
 export ROSLISP_PACKAGE_DIRECTORIES='/home/abderrahim/INF3995-104/embedded/gz_sim_ws/devel/share/common-lisp'
 export ROS_PACKAGE_PATH="/home/abderrahim/INF3995-104/embedded/gz_sim_ws/src:$ROS_PACKAGE_PATH"
---
+++ embedded/gz_sim_ws/build/limo/limo_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/abderrahim/INF3995-104/embedded/gz_sim_ws/src/limo/limo_description
+# Build directory: /home/abderrahim/INF3995-104/embedded/gz_sim_ws/build/limo/limo_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
